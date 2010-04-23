fad0651e5185b33c63b009672cbf131718f721cb
Rahsun McAfee <rahsun@rahsunmcafee.com>
2010-04-24 00:11:38 +0800

    suppot for rails 3

--- lib/chargify_api_ares.rb
+++ lib/chargify_api_ares.rb
@@ -17,8 +17,9 @@ end
 
 # Version check
 module Chargify
-  ARES_VERSIONS = ['2.3.4', '2.3.5']
+  ARES_VERSIONS = ['2.3.4', '2.3.5','3.0.0.beta','3.0.0.beta2','3.0.0.beta3']
 end
+
 require 'active_resource/version'
 unless Chargify::ARES_VERSIONS.include?(ActiveResource::VERSION::STRING)
   abort <<-ERROR
